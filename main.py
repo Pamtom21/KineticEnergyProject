64891a9469db3475d0aa2f103b49cc97ea42cc1a
MatiasV4squez <mvasquez2023@alu.uct.cl>
2023-06-15 00:47:57 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import tkinter as tk 
+
+Ventana = tk.Tk()
+Ventana.geometry('800x640')
+
+Ingreso1 = tk.Entry(Ventana, text ='0')
+Ingreso1.pack()
+
+Ingreso2 = tk.Entry(Ventana, text= '1')
+Ingreso2.pack() 
+
+
+def Formula():
+    try:
+        dat1 = float(Ingreso1.get())
+        dat2 = float(Ingreso2.get())
+        energiaC = (0.5*dat1)
+        energiaC1= energiaC*dat2**2
+        print(energiaC1)
+    except ValueError:
+        pass 
+
+
+botonPr = tk.Button(Ventana, text = 'Calcular', command = Formula)
+botonPr.pack()
+
+foto = tk.PhotoImage(file = 'fondo.gif')
+l = tk.Label(image=foto)
+l.pack()
+
+
+Ventana.mainloop()
