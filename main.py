0a60f2302a7cfd2887c3daef8a30f0d7ce3192fe
MatiasV4squez <mvasquez2023@alu.uct.cl>
2023-06-21 16:03:24 -0400

    update

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ Ventana.geometry('320x320')
 Ventana.title('Energia Cinetica')
 Ventana.configure(background= 'dark slate blue')
 
-
+Texto1 = tk.Label(Ventana, text= 'ingrese masa').place(x=92,y=15)
 Ingreso1 = tk.Entry(Ventana, text ='1')#
 Ingreso1.place(x=92,y=40)
 
-
+Texto2 = tk.Label(Ventana, text= 'ingrese velocidad').place(x=92, y=65)
 Ingreso2 = tk.Entry(Ventana, text= '2')#.place(x=92,y=40)
 Ingreso2.place(x=92,y=90) 
 
@@ -21,7 +21,7 @@ def Formula():
         dat1 = float(Ingreso1.get())
         dat2 = float(Ingreso2.get())
         energiaC = (0.5*dat1)*dat2**2
-        resultado = tk.Label(Ventana,text= 'La energia es : '+str(energiaC)+' Joule')#.place(x=92,y=170)
+        resultado = tk.Label(Ventana,text= 'La energia es : '+str(energiaC)+' Joule').place(x=92,y=170)
     
         print(energiaC)
     except ValueError:
@@ -30,12 +30,6 @@ def Formula():
         Cajaemergente.pack()
 
 botonPr = tk.Button(Ventana, text = 'Ingrese masa', command = Formula).place(x=110,y=130)
-#botonPr.pack()
-#if botonPr == True:
-#    resultado.replace(x=92,y=170)
-
-
-
 
 
 Ventana.mainloop()
