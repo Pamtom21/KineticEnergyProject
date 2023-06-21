9a0c5a88d21c25169b653264c3d9a04c95ae9ee9
MatiasV4squez <mvasquez2023@alu.uct.cl>
2023-06-21 14:47:42 -0400

    atuclizacion para maty_test

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
 import tkinter as tk 
 
+
+
 Ventana = tk.Tk()
-Ventana.geometry('800x640')
+Ventana.geometry('320x320')
+Ventana.title('Energia Cinetica')
+Ventana.configure(background= 'dark slate blue')
+
+
 
 Ingreso1 = tk.Entry(Ventana, text ='0')
 Ingreso1.pack()
@@ -15,18 +21,18 @@ def Formula():
     try:
         dat1 = float(Ingreso1.get())
         dat2 = float(Ingreso2.get())
-        energiaC = (0.5*dat1)
-        energiaC1= energiaC*dat2**2
-        print(energiaC1)
+        energiaC = (0.5*dat1)*dat2**2
+        resultado = tk.Label(Ventana,text= 'La energia es : '+str(energiaC))
+        resultado.pack()
+        print(energiaC)
     except ValueError:
         pass 
 
 botonPr = tk.Button(Ventana, text = 'Ingrese masa', command = Formula)
 botonPr.pack()
 
-foto = tk.PhotoImage(file = 'fondo.gif')
-l = tk.Label(image=foto)
-l.pack()
+
+
 
 
 Ventana.mainloop()
