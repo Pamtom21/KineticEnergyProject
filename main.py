7eafdd9e18dfa0c9965291fc276af8baca9f8b7a
Pamtom21 <joluis.2.11.04@gmail.com>
2023-06-23 00:32:57 -0400

    subo codigo de ventana

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-
-import tkinter as tk 
+import tkinter as tk
 
 Ventana = tk.Tk()
-Ventana.geometry('800x640')
-
-Ingreso1 = tk.Entry(Ventana, text ='0')
-Ingreso1.pack()
-
-Ingreso2 = tk.Entry(Ventana, text= '1')
-Ingreso2.pack() 
+Ventana.geometry('320x320')
+Ventana.title('Energia Cinetica')
+Ventana.configure(background= 'dark slate blue')
 
 
-def Formula():
+n1 = tk.Entry(Ventana, text ='1')#masa
+n1.place(x=92,y=40)
+
+
+n2 = tk.Entry(Ventana, text= '2')#velocidad
+n2.place(x=92,y=90) 
+
+
+def resolver(*_):
+    a= n1.get()#masa
+    b= n2.get()#velocidad
+
+    if not (a:= n1.get()) or not (b:= n2.get()):
+        return
     try:
-        dat1 = float(Ingreso1.get())
-        dat2 = float(Ingreso2.get())
-        energiaC = (0.5*dat1)
-        energiaC1= energiaC*dat2**2
-        print(energiaC1)
+        a, b = float(a), float(b)
+
     except ValueError:
-        pass 
-
-
-botonPr = tk.Button(Ventana, text = 'Calcular', command = Formula)
-botonPr.pack()
-
-foto = tk.PhotoImage(file = 'fondo.gif')
-l = tk.Label(image=foto)
-l.pack()
-
+        res.set("ERROR")
+    else: res.set(str(0.5*a*(b**2))+' '+'Joule')
+    
+res = tk. StringVar()
+botonPr = tk.Button(Ventana, text = 'CALCULAR', command = resolver ).place(x=110,y=250)
+tk.Label(Ventana, text="\nIngrese Masa(kg)").place(x=92,y=5)
+tk.Label(Ventana, text="\nIngrese Velocidad(m/s)").place(x=92,y=55)
+tk.Label(Ventana, text="\nLa energia cinetica es: ").place(x=92,y=130)
+tk.Entry(
+    Ventana, justify="center", textvariable=res, state="disabled"
+    ).place(x=92,y= 170)
 
 Ventana.mainloop()
