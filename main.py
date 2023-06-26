7113e908c62a3a235e3881320c0d8f92844b6d61
MatiasV4squez <mvasquez2023@alu.uct.cl>
2023-06-25 20:15:25 -0400

    update prefin

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-
-import tkinter as tk
+import tkinter as tk, numpy as np, matplotlib.pyplot as  plt
 from tkinter import messagebox
+
+x = np.arange(-5, 5,0.05)
+y=  np.cos(x)
+
 
 Ventana = tk.Tk()
 Ventana.geometry('320x320')
 Ventana.title('Energia Cinetica')
 Ventana.configure(background= 'dark slate blue')
 
-Texto1 = tk.Label(Ventana, text= 'ingrese masa').place(x=92,y=15)
-Ingreso1 = tk.Entry(Ventana, text ='1')#
-Ingreso1.place(x=92,y=40)
 
-Texto2 = tk.Label(Ventana, text= 'ingrese velocidad').place(x=92, y=65)
-Ingreso2 = tk.Entry(Ventana, text= '2')#.place(x=92,y=40)
-Ingreso2.place(x=92,y=90) 
+n1 = tk.Entry(Ventana, text ='1')#masa
+n1.place(x=92,y=40)
 
 
-def Formula():
+n2 = tk.Entry(Ventana, text= '2')#velocidad
+n2.place(x=92,y=90) 
+
+
+def resolver(*_):
+    a= n1.get()#masa
+    b= n2.get()#velocidad
+
+    if not (a:= n1.get()) or not (b:= n2.get()):
+        return
     try:
-        dat1 = float(Ingreso1.get())
-        dat2 = float(Ingreso2.get())
-        energiaC = (0.5*dat1)*dat2**2
-        resultado = tk.Label(Ventana,text= 'La energia es : '+str(energiaC)+' Joule').place(x=92,y=170)
-    
-        print(energiaC)
+        a, b = float(a), float(b)
+
     except ValueError:
-        pass
         Cajaemergente= messagebox.showerror('Error', 'Coloque numeros')
         Cajaemergente.pack()
+    else: 
+        res.set(str(0.5*a*(b**2)))
+        F = 0.5*a*(b**2)
+        x = np.arange(-5, F, 0.5)
+        y = np.cos(x)
+        plt.plot(x,y)
+        plt.show()
 
-botonPr = tk.Button(Ventana, text = 'Ingrese masa', command = Formula).place(x=110,y=130)
+res = tk. StringVar()
+botonPr = tk.Button(Ventana, text = 'CALCULAR', command = resolver ).place(x=110,y=250)
+tk.Label(Ventana, text="\nIngrese Masa(kg)").place(x=92,y=5)
+tk.Label(Ventana, text="\nIngrese Velocidad(m/s)").place(x=92,y=55)
+tk.Label(Ventana, text="\nLa energia cinetica es: ").place(x=92,y=130)
+tk.Entry(
+    Ventana, justify="center", textvariable=res, state="disabled"
+    ).place(x=92,y= 170)
+
+
+
 
 
 Ventana.mainloop()
-
